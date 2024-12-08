e31f26ce5ac5366b4efe7ca4fabcbcf807087ac6
sonu79934 <yogasritha8@gmail.com>
2024-12-08 14:26:38 +0530

    Add files via upload

---
+++ twopointermergetechnique.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class twopointermergetechnique{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr1[]={3,4,5,6};
+        int arr2[]={9,10,12};
+        int k=0;
+        int temp[]=new int[arr1.length+arr2.length];
+        for(int i=0;i<arr1.length;i++)
+        {
+            temp[k]=arr1[i];
+            k++;
+        }
+        for(int i=0;i<arr2.length;i++)
+        {
+            temp[k]=arr2[i];
+            k++;
+        }
+        System.out.println(Arrays.toString(temp));
+    }
+}
